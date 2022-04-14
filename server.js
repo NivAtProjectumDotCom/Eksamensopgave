81aa65a709d14845ff6964dfc5594aa456f722b5
Taoemil <taoemil@hotmail.com>
2022-04-14 15:43:54 +0200

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Controllers
 // Users
-const userController = require("./src/controller/user-controller");
+const userController = require("./src/controller/user-controllers");
 
 
 const PORT = process.env.PORT || 3000;
