20ef0440942b736dd4b6ec49d43c140f19579cd6
Taoemil <taoemil@hotmail.com>
2022-04-14 15:22:42 +0200

    Tilføjer unit

--- src/context/dbcontext.js
+++ src/context/dbcontext.js
@@ -71,4 +71,37 @@ const executeNonQuery = (query, params) => new Promise(
         conn.connect();
     });
 
+
+
+/* FUNKTION TIL AT INDSÆTTE EN BRUGER IND I DATABASEN. DEN VIRKER, MORTEN BLEV SAT IND :-)
+function executeSQL(){
+    request = new Request("INSERT INTO ProgEksamen.Users (ID, Username, Password, Email, Userlevel, Followed_ads) VALUES (@ID, @Username, @Password, @Email, @Userlevel, @Followed_ads)", function(err){
+    if (err){
+        console.log(err)}})
+        request.addParameter("ID", TYPES.Int, 4);
+            request.addParameter("Username", TYPES.VarChar, 'Morten');
+            request.addParameter("Password", TYPES.VarChar, 'Morten123');
+            request.addParameter("Email", TYPES.VarChar, 'Mortenjensen@gmail.com');
+            request.addParameter("Userlevel", TYPES.Int, 1);
+            request.addParameter("Followed_ads", TYPES.Int, 3);
+
+
+
+    connection.execSql(request)
+    var counter = 1
+    response = {}
+    request.on('row', function(columns){
+        response[counter] = {}
+        columns.forEach(function(column){
+            response[counter][column.metadata.colName] = column.value
+        });
+        counter += 1
+    });
+    return response
+};
+
+
+*/
+
+
 module.exports.executeNonQuery = executeNonQuery;
