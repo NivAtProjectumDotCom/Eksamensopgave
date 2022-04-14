3ac67cb15e32cf2e6a97516500480c6d467722bd
helenarosendahl <91500432+helenarosendahl@users.noreply.github.com>
2022-04-14 15:24:01 +0200

    model ads

---
+++ src/models/Ads.js
@@ -0,0 +1,13 @@
+class Add {
+    constructor(id, addname, price, category, location, img)  {
+        this.id = id,
+        this.addname = addname,
+        this.price = price,
+        this.category = category,
+        this.location = location,
+        this.img = img
+    }
+    
+}
+
+module.exports = Add
