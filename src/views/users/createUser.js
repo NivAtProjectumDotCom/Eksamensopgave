1434b820b745834619786b7822d96253b56afe3e
sgnielsen <sgnielsen11@gmail.com>
2022-04-29 13:26:28 +0200

    create user

--- src/views/users/createUser.js
+++ src/views/users/createUser.js
@@ -1,21 +1,27 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
     // Event on button click
-    document.getElementById("createUserButton").addEventListener("click", (event) => {
-        event.preventDefault()
+    document.getElementById("createUserButton").addEventListener("click", (e) => {
+        e.preventDefault();
+
         // Collect values for API
-        let usernameVal = document.getElementById("usernameInput").value;
-        let passwordVal = document.getElementById("passwordInput").value;
-        let emailVal = document.getElementById("EmailInput").value;
+        let usernameVal = document.getElementById("usernameInput");
+        let passwordVal = document.getElementById("passwordInput");
+        let emailVal = document.getElementById("EmailInput");
+
+        let form = document.getElementById("form");
+
+        if(form.checkValidity() == false) {
+            form.reportValidity();
+            return;
+        }
   
         let user = {
-            username: usernameVal,
-            password: passwordVal,
-            email: emailVal,
-    
- 
+            username: usernameVal.value,
+            password: passwordVal.value,
+            email: emailVal.value,
         };
-        console.log('teste')
+
         // Call API 
         fetch(location.origin + "/users/create", {
             method: "POST",
