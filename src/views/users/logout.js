5fd130eafd444868b4ac12d8b993609507cbe64e
helenarosendahl <91500432+helenarosendahl@users.noreply.github.com>
2022-05-02 10:31:03 +0200

    logud krav

--- src/views/users/logout.js
+++ src/views/users/logout.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const user = localStorage.getItem("user"); //Vi finder brugeren fra localstorage
     
     if (!user) { //Hvis brugeren ikke findes
-        location.href = "/login.html" // skal den føres videre til logind siden. 
+        window.alert("Brugeren eksisterer ikke"); 
     } 
 
     document.getElementById("logout").addEventListener("click", (e) => { //Når knappen logud bliver klikket
         localStorage.removeItem("user"); //Bliver brugeren i localstorage fjernet i browseren. 
-        location.href = "/login.html" // Og brugeren bliver ført ud igen til logind siden. 
+        location.href = "/" // Og brugeren bliver ført ud igen til logind siden. 
     
     })}
 )
